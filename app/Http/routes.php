1f2ec36a878caa2638cbd7917d8d1e6247e6fb40
Martin Ruza <matoruza@gmail.com>
2019-09-13 14:22:40 +0200

    admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,19 @@
 |
 */
 
+use Illuminate\Support\Facades\Route;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('/admin', function (){
+
+    return view('admin.index');
+});
+
+Route::resource('admin/users', 'AdminUsersController');
