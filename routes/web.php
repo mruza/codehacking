73ff05b21517ff9f84d2ee4e1e7cf570ebae5a29
Martin Ruza <martin.ruza@01people.com>
2019-10-22 14:45:01 +0200

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -32,15 +32,53 @@ Route::group(['middleware'=>'admin'], function (){
         return view('admin.index');
     });
 
-    Route::resource('admin/users', 'AdminUsersController');
+    Route::resource('admin/users', 'AdminUsersController', ['names' => [
+        'index' => 'admin.users.index',
+        'create' => 'admin.users.create',
+        'store' => 'admin.users.store',
+        'edit' => 'admin.users.edit',
+        'update' => 'admin.users.update',
+        'destroy' => 'admin.users.destroy',
+    ]]);
 
-    Route::resource('admin/posts', 'AdminPostsController');
 
-    Route::resource('admin/categories', 'AdminCategoriesController');
+    Route::resource('admin/posts', 'AdminPostsController', ['names' => [
+        'index' => 'admin.posts.index',
+        'show' => 'admin.posts.show',
+        'create' => 'admin.posts.create',
+        'store' => 'admin.posts.store',
+        'edit' => 'admin.posts.edit',
+        'update' => 'admin.posts.update',
+        'destroy' => 'admin.posts.destroy',
+    ]]);
 
-    Route::resource('admin/media', 'AdminMediasController');
+    Route::resource('admin/categories', 'AdminCategoriesController', ['names' => [
+        'index' => 'admin.categories.index',
+        'create' => 'admin.categories.create',
+        'store' => 'admin.categories.store',
+        'edit' => 'admin.categories.edit',
+        'update' => 'admin.categories.update',
+        'destroy' => 'admin.categories.destroy',
+    ]]);
 
-    Route::resource('admin/comments', 'PostCommentsController');
+    Route::resource('admin/media', 'AdminMediasController', ['names' => [
+        'index' => 'admin.media.index',
+        'create' => 'admin.media.create',
+        'store' => 'admin.media.store',
+        'edit' => 'admin.media.edit',
+        'update' => 'admin.media.update',
+        'destroy' => 'admin.media.destroy',
+    ]]);
+
+    Route::resource('admin/comments', 'PostCommentsController', ['names' => [
+        'index' => 'admin.comments.index',
+        'show' => 'admin.comments.show',
+        'create' => 'admin.comments.create',
+        'store' => 'admin.comments.store',
+        'edit' => 'admin.comments.edit',
+        'update' => 'admin.comments.update',
+        'destroy' => 'admin.comments.destroy',
+    ]]);
 
     Route::resource('admin/comment/replies', 'CommentRepliesController');
 
